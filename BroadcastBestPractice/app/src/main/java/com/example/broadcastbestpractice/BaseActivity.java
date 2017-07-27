27814c5ad267a43e984a26c8042eec77203672a5
jiangtingting <18792898826@163.com>
2017-07-27 09:23:46 +0800

    强制下线

--- BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/BaseActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/BaseActivity.java
@@ -1,8 +1,14 @@
 package com.example.broadcastbestpractice;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -20,11 +26,45 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.broadcastbestpractice.IREALLYLIKEYOU");
+        mMyReceiver = new MyReceiver();
+        registerReceiver(mMyReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mMyReceiver != null) {
+            unregisterReceiver(mMyReceiver);
+            mMyReceiver = null;
+        }
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         ActivityCollector.removeActivity(this);
     }
 
-    private class MyReceiver {
+    private class MyReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setMessage("你已被强制下线");
+            builder.setCancelable(false);
+            builder.setNegativeButton("ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    ActivityCollector.finishAll();
+                    Intent intent = new Intent(context, LoginActivity.class);
+                    context.startActivity(intent);
+                }
+            });
+            builder.show();
+        }
     }
 }
