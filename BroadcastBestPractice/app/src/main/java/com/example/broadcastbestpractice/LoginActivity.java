e52285e290e58887b8fd470e4a67c3e9b8bfa6af
jiangtingting <18792898826@163.com>
2017-08-23 11:31:53 +0800

    add remember password

--- BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.broadcastbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,6 +18,10 @@ public class LoginActivity extends BaseActivity {
     private EditText passwordEdit;
     private Button button;
 
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
+    private CheckBox mCheckBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +29,34 @@ public class LoginActivity extends BaseActivity {
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
         button = (Button) findViewById(R.id.button);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mCheckBox = (CheckBox) findViewById(R.id.checkbox);
+        boolean flag = mPreferences.getBoolean("flag", false);
+
+        if (flag) {
+            accountEdit.setText(mPreferences.getString("account", ""));
+            passwordEdit.setText(mPreferences.getString("password", ""));
+            mCheckBox.setChecked(true);
+        }
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String account = accountEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
                 if (account.equals("jiangtingting") && password.equals("zengshuaizhi")) {
+                    mEditor = mPreferences.edit();
+                    if (mCheckBox.isChecked()) {
+                        mEditor.putBoolean("flag", true);
+                        mEditor.putString("account", account);
+                        mEditor.putString("password", password);
+                    } else {
+                        mEditor.clear();
+                    }
+                    mEditor.apply();
+
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
